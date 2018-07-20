6e5972afc5a3f5bf6e0c68da2f74c03f3f11556d
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-20 23:14:33 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,7 +27,9 @@ class SongsController < ApplicationController
   def new
     if params[:artist_id] && !Artist.exists?(params[:artist_id])
       redirect_to artists_path, alert: "Artist not found."
-    @song = Song.new
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -69,4 +71,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
