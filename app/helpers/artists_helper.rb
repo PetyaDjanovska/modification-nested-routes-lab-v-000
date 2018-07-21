22df9ce35317beb92ac7d3baff1ecf58ba5d3204
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-21 00:07:36 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,7 +5,7 @@ module ArtistsHelper
 
   def artist_select(song,artist)
     if song.artist.nil?
-      render
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       hidden_field_tag "song[artist_id]", song.artist_id
     end
